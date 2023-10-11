8c16ed9fd497dafcb1fd470663456d1720cb9c8b
Nandu <nandu@DESKTOP-2ULHSVQ>
2023-10-11 12:25:33 +0530

    Finsih

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,57 @@ async fn main() -> Result<(), fantoccini::error::CmdError> {
 
     c.wait().for_element(Locator::Css(".lkflLS")).await?;
     let elements = c.find_all(Locator::Css(".lkflLS")).await?;
+    let mut links = vec![];
 
     for i in elements {
-        let link = i.attr("href").await.unwrap();
-        let reponse = reqwest::blocking::get(link);
+        links.push(i.attr("href").await.unwrap().unwrap())
+    }
+
+    println!(
+        "Name,Place,Registration Starts,Registration End,Hackathon Starts,Hackathon Ends,Link"
+    );
+
+    for link in links {
+        c.goto(&link)
+            .await
+            .expect(&format!("Could not access {}", link));
+        thread::sleep(Duration::from_secs(5));
+
+        let title = c.find(Locator::Css(".dxzFsX")).await?.text().await?;
+        let location: String = match c.find(Locator::Css(".jAZTsD + .jAZTsD > p + p")).await {
+            Ok(e) => e.text().await?,
+            Err(_) => "None Given".to_string(),
+        };
+
+        c.goto(&format!("{}schedule", link))
+            .await
+            .expect("Schedule could not be reached");
+        thread::sleep(Duration::from_secs(5));
+
+        let date_elements = c.find_all(Locator::Css("[name=\"calendar\"] + p")).await?;
+        let datetime_elements = date_elements
+            .iter()
+            .zip(c.find_all(Locator::Css("[name=\"clock\"] + p")).await?);
+
+        let mut datetimes = Vec::new();
+
+        for (date, time) in datetime_elements {
+            datetimes.push((date.text().await?, time.text().await?));
+        }
+
+        let mut rec = format!("{},\"{}\",", title, location);
+
+        if datetimes.is_empty() {
+            rec.push_str("None Given,None Given,None Given,None Given,");
+        } else {
+            for j in 0..4 {
+                rec.push_str(&format!("{} {},", datetimes[j].0, datetimes[j].1))
+            }
+        }
+
+        rec.push_str(&link);
+
+        println!("{rec}");
     }
 
     c.close().await
